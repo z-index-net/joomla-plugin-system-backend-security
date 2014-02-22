297055a65a5b23a6a71755a44a6c5b0aa32d5a94
b2un0 <branko.wilhelm@gmail.com>
2014-02-22 23:08:40 +0100

    some changes

--- backend_security.php
+++ backend_security.php
@@ -2,32 +2,34 @@
 /**
  * @author     mediahof, Kiel-Germany
  * @link       http://www.mediahof.de
- * @copyright  Copyright (C) 2011 - 2013 mediahof. All rights reserved.
+ * @copyright  Copyright (C) 2013 - 2014 mediahof. All rights reserved.
  * @license    GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
 defined('_JEXEC') or die;
 
-class plgSystemBackend_security extends JPlugin {
+class plgSystemBackend_security extends JPlugin
+{
 
-    function __construct(&$subject, $config) {
+    function __construct(&$subject, $config)
+    {
         parent::__construct($subject, $config);
-        
+
         $app = JFactory::getApplication();
         $session = JFactory::getSession();
-        
-        if(!$app->isAdmin()) { 
+
+        if (!$app->isAdmin()) {
             return;
         }
-        
+
         $key = $session->get('backend_security');
-        
-        if(empty($key)) {
-            if(JFactory::getUser()->guest && $this->params->get('key') != $_SERVER['QUERY_STRING']) {
-                $app->redirect(JURI::root());
+
+        if (empty($key)) {
+            if (JFactory::getUser()->guest && $this->params->get('key') != $_SERVER['QUERY_STRING']) {
+                $app->redirect(JUri::root());
             } else {
                 $session->set('backend_security', 1);
-        }
+            }
         }
     }
 }
